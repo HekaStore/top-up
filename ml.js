593eea45a85c282fc4cc38a4b367dceaf831fa5a
HekaStore <118747697+HekaStore@users.noreply.github.com>
2023-04-11 14:19:47 +0700

    Update ml.js

--- ml.js
+++ ml.js
@@ -1,20 +1,151 @@
-document.getElementById('linkwa1').style.display = "none";
-document.getElementById('linkwa2').style.display = "none";
-document.getElementById('linkwa3').style.display = "none";
-document.getElementById('linkwa4').style.display = "none";
-document.getElementById('linkwa5').style.display = "none";
-document.getElementById('linkwa6').style.display = "none";
-document.getElementById('linkwa7').style.display = "none";
-document.getElementById('linkwa8').style.display = "none";
-document.getElementById('linkwa9').style.display = "none";
-document.getElementById('linkwa10').style.display = "none";
-document.getElementById('linkwa11').style.display = "none";
-document.getElementById('linkwa12').style.display = "none";
-document.getElementById('linkwa13').style.display = "none";
-document.getElementById('linkwa14').style.display = "none";
-document.getElementById('linkwa15').style.display = "none";
-document.getElementById('linkwa16').style.display = "none";
-document.getElementById('linkwa17').style.display = "none";
-document.getElementById('linkwa18').style.display = "none";
-document.getElementById('linkwa19').style.display = "none";
-document.getElementById('linkwa20').style.display = "none";
+//SIDE-BAR
+const sideBar = document.getElementById('sideBar');
+const layarHitam = document.getElementById('layarHitam');
+sideBar.style.display = 'none';
+layarHitam.style.display = 'none';
+
+
+//beli
+function submit() {
+     let masukan = document.getElementById('gameid');
+     if(masukan.value == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }else {alert('Klik "OK" Jika Anda Benar-Benar Membeli ini')}
+}
+function belimember1(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20Membership%20Mingguan%20Free%20Fire:%2030.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function belimember2(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20Membership%20Bulanan%20Free%20Fire:%2090.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli1(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2020%20Diamond%20Free%20Fire:%204.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli2(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2030%20Diamond%20Free%20Fire:%206.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli3(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2050%20Diamond%20Free%20Fire:%208.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli4(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2070%20Diamond%20Free%20Fire:%2010.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli5(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20100%20Diamond%20Free%20Fire:%2015.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli6(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20120%20Diamond%20Free%20Fire:%2018.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli7(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20140%20Diamond%20Free%20Fire:%2020.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli8(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20180%20Diamond%20Free%20Fire:%2028.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli9(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20210%20Diamond%20Free%20Fire:%2032.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli10(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20250%20Diamond%20Free%20Fire:%2038.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli11(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20355%20Diamond%20Free%20Fire:%2050.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli12(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20425%20Diamond%20Free%20Fire:%2060.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}function beli13(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20475%20Diamond%20Free%20Fire:%2068.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli14(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20500%20Diamond%20Free%20Fire:%2073.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli15(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20545%20Diamond%20Free%20Fire:%2077.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+     
