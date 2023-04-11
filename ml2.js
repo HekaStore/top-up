49aa00d8e4c23f8f71626c18ba09247e2a40af21
HekaStore <118747697+HekaStore@users.noreply.github.com>
2023-04-11 14:19:14 +0700

    Add files via upload

---
+++ ml2.js
@@ -0,0 +1,151 @@
+//SIDE-BAR
+const sideBar = document.getElementById('sideBar');
+const layarHitam = document.getElementById('layarHitam');
+sideBar.style.display = 'none';
+layarHitam.style.display = 'none';
+
+
+//beli
+function submit() {
+     let masukan = document.getElementById('gameid');
+     if(masukan.value == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }else {alert('Klik "OK" Jika Anda Benar-Benar Membeli ini')}
+}
+function belimember1(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20Weekly%20Diamond%20Pass%201x%20Mobile%20Legend:%2030.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function belimember2(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20Weekly%20Diamond%20Pass%202x%20Mobile%20Legend:%2060.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli1(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2014%20Diamond%20Mobile%20Legend:%204.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli2(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2028%20Diamond%20Mobile%20Legend:%208.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli3(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2042%20Diamond%20Mobile%20Legend:%2012.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli4(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2070%20Diamond%20Mobile%20Legend:%2019.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli5(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%2086%20Diamond%20Mobile%20Legend:%2022.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli6(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20140%20Diamond%20Mobile%20Legend:%2037.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli7(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20172%20Diamond%20Mobile%20Legend:%2043.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli8(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20257%20Diamond%20Mobile%20Legend:%2062.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli9(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20284%20Diamond%20Mobile%20Legend:%2068.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli10(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20355%20Diamond%20Mobile%20Legend:%2085.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli11(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20429%20Diamond%20Mobile%20Legend:%20102.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli12(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20514%20Diamond%20Mobile%20Legend:%20122.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}function beli13(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20600%20Diamond%20Mobile%20Legend:%20140.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli14(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20500%20Diamond%20Mobile%20Legend:%2073.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+function beli15(){
+     let ID = document.getElementById('gameid').value;
+     document.getElementById('beli').href = 'https://wa.me/6289514433486?text=Beli%20Twilight%20Pass%20Mobile%20Legend:%20135.000%0aUser%20ID:%20' + ID;
+     if(ID == '') {
+          alert('Ketik ID Game Terlebih Dahulu !!!');
+          document.getElementById('beli').href = '#';
+     }
+}
+     
