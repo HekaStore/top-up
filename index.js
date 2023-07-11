2696108787ef3a5e264fe5e20785ef6100d2e8e7
HekaStore <118747697+HekaStore@users.noreply.github.com>
2023-07-11 15:08:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ function nothing() {
      alert('Webnya masih dalam perbaikan, silahkan membeli manual dengan mengechat adminnya :)');
 }
 
+//slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementById("slide-show-logomain").getElementsByTagName("img");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change slide every 2 seconds
+}
